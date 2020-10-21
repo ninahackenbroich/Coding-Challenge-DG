1b7876a466cffe43d88fd8b8634d69a8cebe42e8
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-10-21 11:58:56 +0200

    last status view is not done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_090340) do
+ActiveRecord::Schema.define(version: 2020_10_21_095334) do
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_10_21_090340) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "lunchgroups", force: :cascade do |t|
+  create_table "groups", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
